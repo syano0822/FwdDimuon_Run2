b9f28e63cedf4049e6a5993740cc2eb9d7b613a3
syano0822 <satoshi.yano@cern.ch>
2022-04-19 11:05:04 +0200

    Add AddTaskAODTrackPairMC.C

---
+++ AddTaskAODTrackPairMC.C
@@ -0,0 +1,107 @@
+AliAnalysisTaskAODTrackPairMC* AddTaskAODTrackPairMC(UInt_t offlineTriggerMask = AliVEvent::kAny,
+						     float min_vtxz =-10,
+						     float max_vtxz = 10,
+						     int min_vtx_cont = 1,
+						     float min_pair_rap = -4.0,
+						     float max_pair_rap = -2.5,
+						     string multi_method="SPDTracklets",
+						     bool onPURej = true,
+						     bool onLBcut = true,
+						     bool onMuEtaCut = true,
+						     bool onMuThetaAbsCut = true,
+						     bool onMuMatchAptCut = true,
+						     bool onMuMatchLptCut = true,
+						     bool onMuMatchHptCut = false,
+						     bool onMuChi2Cut = true,
+						     bool onMuPdcaCut = true,
+						     bool isMC=false,
+						     bool isSelectEvt=true,
+						     int paircuttype=1,
+						     double min_pairtrackptcut=0.5,
+						     bool onMixingAnalysis=false)
+
+{
+    
+  AliMuonTrackCuts* fMuonTrackCuts = new AliMuonTrackCuts("StandardMuonTrackCuts", "StandardMuonTrackCuts");
+  fMuonTrackCuts->SetIsMC(isMC);
+  fMuonTrackCuts->SetAllowDefaultParams(true);
+  int selectionMask = 0;
+  if(onMuEtaCut) selectionMask |= AliMuonTrackCuts::kMuEta;
+  if(onMuThetaAbsCut) selectionMask |=AliMuonTrackCuts::kMuThetaAbs;
+  if(onMuMatchAptCut) selectionMask |=AliMuonTrackCuts::kMuMatchApt;
+  if(onMuMatchLptCut) selectionMask |=AliMuonTrackCuts::kMuMatchLpt;
+  if(onMuMatchHptCut) selectionMask |=AliMuonTrackCuts::kMuMatchHpt;
+  if(onMuPdcaCut) selectionMask |=AliMuonTrackCuts::kMuPdca;
+  if(onMuChi2Cut) selectionMask |=AliMuonTrackCuts::kMuTrackChiSquare;    
+  fMuonTrackCuts->SetFilterMask(selectionMask);
+  
+  TFile* input = TFile::Open("./DownScale_Run2_CTP.root");
+
+  AliAnalysisTaskAODTrackPairUtils *utils = new AliAnalysisTaskAODTrackPairUtils();
+  utils->setMC(isMC);
+  utils->setEvtSelection(isSelectEvt);
+  utils->setDownScalingHist(input);
+  utils->setVertexCut(min_vtxz,max_vtxz,min_vtx_cont);
+  utils->setPairRapidityCut(min_pair_rap,max_pair_rap);
+  utils->setPileupRejectionCut(onPURej);
+  utils->setLocalBoardCut(onLBcut);
+  utils->setMultiEstimateMethod(multi_method);
+  utils->setMuonTrackCut(fMuonTrackCuts);
+  utils->setPairKinematicCut(paircuttype,min_pairtrackptcut);
+  
+  AliAnalysisManager *mgr = AliAnalysisManager::GetAnalysisManager();
+  if (!mgr) {
+    ::Error("AddTaskAODMuonEventSelection", "No analysis manager to connect to");
+    return NULL;
+  }
+  
+  if (!mgr->GetInputEventHandler()) {
+    ::Error("AddTaskAODMuonEventSelection", "This task requires an input event handler");
+    return NULL;
+  }
+    
+  AliAnalysisTaskAODTrackPairMC* task = new AliAnalysisTaskAODTrackPairMC("dimuonMC");
+  if(isSelectEvt){
+    task->SelectCollisionCandidates(offlineTriggerMask);
+  }
+  task->setUtils(utils);
+  task->setEvtMixingTrackDepth(100);
+  task->setEvtMixingPoolSize(100);
+  task->setEvtMixingReadyFraction(0.1);
+  task->setEvtMixingPoolVtxZ(true);
+  task->setEvtMixingPoolCent(true);
+  task->setEvtMixingPoolPsi(true);
+  task->setMixingAnalysis(onMixingAnalysis);
+  task->setMC(isMC);
+  mgr->AddTask(task);
+
+  cout<< min_vtxz <<endl;
+  cout<< max_vtxz <<endl;
+  cout<<  min_vtx_cont <<endl;
+  cout<< min_pair_rap <<endl;
+  cout<< max_pair_rap <<endl;
+  cout<< multi_method <<endl;
+  cout<< onPURej <<endl;
+  cout<< onLBcut <<endl;
+  cout<< onMuEtaCut <<endl;
+  cout<< onMuThetaAbsCut <<endl;
+  cout<< onMuMatchAptCut <<endl;
+  cout<< onMuMatchLptCut <<endl;
+  cout<< onMuMatchHptCut <<endl;
+  cout<< onMuChi2Cut <<endl;
+  cout<< onMuPdcaCut <<endl;
+  cout<< isMC <<endl;
+  cout<< isSelectEvt <<endl;
+  cout<< paircuttype <<endl;
+  cout<< min_pairtrackptcut <<endl;
+  cout<< onMixingAnalysis <<endl;
+
+  AliAnalysisDataContainer *cinput  = mgr->GetCommonInputContainer();
+  AliAnalysisDataContainer *coutput1 = mgr->CreateContainer("dimuon",TList::Class(),AliAnalysisManager::kOutputContainer,"Dimuon.root");
+    
+  mgr->ConnectInput(task,0,cinput);
+  mgr->ConnectOutput(task,1,coutput1);
+
+  return task;
+
+}
